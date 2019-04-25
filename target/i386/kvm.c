b625799047fbcb97675937f21a6b674b6bb84b1d
Tom Lendacky <thomas.lendacky@amd.com>
2019-04-25 15:46:01 -0500

    Make the call to kvm_get_apic() earlier

--- target/i386/kvm.c
+++ target/i386/kvm.c
@@ -2869,6 +2869,10 @@ int kvm_arch_get_registers(CPUState *cs)
     if (ret < 0) {
         goto out;
     }
+    ret = kvm_get_apic(cpu);
+    if (ret < 0) {
+        goto out;
+    }
     /*
      * KVM_GET_MPSTATE can modify CS and RIP, call it before
      * KVM_GET_REGS and KVM_GET_SREGS.
@@ -2894,10 +2898,6 @@ int kvm_arch_get_registers(CPUState *cs)
         goto out;
     }
     ret = kvm_get_msrs(cpu);
-    if (ret < 0) {
-        goto out;
-    }
-    ret = kvm_get_apic(cpu);
     if (ret < 0) {
         goto out;
     }
