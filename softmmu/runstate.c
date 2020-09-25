c1b45c0f74820dffbc28625c9c44f603f44b76ee
Tom Lendacky <thomas.lendacky@amd.com>
2020-09-25 14:12:57 -0500

    sev/i386: Don't allow a system reset under an SEV-ES guest
    An SEV-ES guest does not allow register state to be altered once it has
    been measured. When an SEV-ES guest issues a reboot command, Qemu will
    reset the vCPU state and resume the guest. This will cause failures under
    SEV-ES. Prevent that from occuring by introducing an arch-specific
    callback that returns a boolean indicating whether vCPUs are resettable.
    
    Cc: Peter Maydell <peter.maydell@linaro.org>
    Cc: Aurelien Jarno <aurelien@aurel32.net>
    Cc: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Cc: Aleksandar Rikalo <aleksandar.rikalo@syrmia.com>
    Cc: David Gibson <david@gibson.dropbear.id.au>
    Cc: David Hildenbrand <david@redhat.com>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- softmmu/runstate.c
+++ softmmu/runstate.c
@@ -523,8 +523,11 @@ void qemu_system_guest_crashloaded(GuestPanicInformation *info)
 
 void qemu_system_reset_request(ShutdownCause reason)
 {
-    if (reboot_action == REBOOT_ACTION_SHUTDOWN &&
-        reason != SHUTDOWN_CAUSE_SUBSYSTEM_RESET) {
+    if (!cpus_are_resettable()) {
+        error_report("cpus are not resettable, terminating");
+        shutdown_requested = reason;
+    } else if (reboot_action == REBOOT_ACTION_SHUTDOWN &&
+               reason != SHUTDOWN_CAUSE_SUBSYSTEM_RESET) {
         shutdown_requested = reason;
     } else {
         reset_requested = reason;
