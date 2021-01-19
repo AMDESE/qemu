0285e0272f8a143ef7b3eeee1eb2abea6e68e622
Brijesh Singh <Brijesh.Singh@amd.com>
2021-01-19 23:43:11 +0000

    target/i386: sev: add support to encrypt the outgoing page
    The sev_save_outgoing_page() provide the implementation to encrypt the
    guest private pages during the transit. The routines uses the SEND_START
    command to create the outgoing encryption context on the first call then
    uses the SEND_UPDATE_DATA command to encrypt the data before writing it
    to the socket. While encrypting the data SEND_UPDATE_DATA produces some
    metadata (e.g MAC, IV). The metadata is also sent to the target machine.
    After migration is completed, we issue the SEND_FINISH command to transition
    the SEV guest state from sending to unrunnable state.
    
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>

--- include/sysemu/sev.h
+++ include/sysemu/sev.h
@@ -20,6 +20,8 @@ void *sev_guest_init(const char *id);
 int sev_encrypt_data(void *handle, uint8_t *ptr, uint64_t len);
 int sev_save_setup(void *handle, const char *pdh, const char *plat_cert,
                    const char *amd_cert);
+int sev_save_outgoing_page(void *handle, QEMUFile *f, uint8_t *ptr,
+                           uint32_t size, uint64_t *bytes_sent, Error **errp);
 int sev_inject_launch_secret(const char *hdr, const char *secret,
                              uint64_t gpa, Error **errp);
 int sev_es_save_reset_vector(void *handle, void *flash_ptr,
