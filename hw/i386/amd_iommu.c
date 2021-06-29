880a89527478c8e52947ddde7da6cca917031077
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2021-06-28 19:56:04 -0500

    hw/i386/amd_iommu: Refactor helper functions into amd_iommu_help.h/c file
    So that the code can be reused for implementing AMD vIOMMU support.
    There is no functional change.
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- hw/i386/amd_iommu.c
+++ hw/i386/amd_iommu.c
@@ -26,6 +26,7 @@
 #include "hw/pci/pci_bus.h"
 #include "migration/vmstate.h"
 #include "amd_iommu.h"
+#include "amd_iommu_helper.h"
 #include "qapi/error.h"
 #include "qemu/error-report.h"
 #include "hw/i386/apic_internal.h"
@@ -73,82 +74,6 @@ typedef struct AMDVIIOTLBEntry {
     uint64_t translated_addr;   /* translated address  */
     uint64_t page_mask;         /* physical page size  */
 } AMDVIIOTLBEntry;
-
-/* configure MMIO registers at startup/reset */
-static void amdvi_set_quad(AMDVIState *s, hwaddr addr, uint64_t val,
-                           uint64_t romask, uint64_t w1cmask)
-{
-    stq_le_p(&s->mmior[addr], val);
-    stq_le_p(&s->romask[addr], romask);
-    stq_le_p(&s->w1cmask[addr], w1cmask);
-}
-
-static uint16_t amdvi_readw(AMDVIState *s, hwaddr addr)
-{
-    return lduw_le_p(&s->mmior[addr]);
-}
-
-static uint32_t amdvi_readl(AMDVIState *s, hwaddr addr)
-{
-    return ldl_le_p(&s->mmior[addr]);
-}
-
-static uint64_t amdvi_readq(AMDVIState *s, hwaddr addr)
-{
-    return ldq_le_p(&s->mmior[addr]);
-}
-
-/* internal write */
-static void amdvi_writeq_raw(AMDVIState *s, hwaddr addr, uint64_t val)
-{
-    stq_le_p(&s->mmior[addr], val);
-}
-
-/* external write */
-static void amdvi_writew(AMDVIState *s, hwaddr addr, uint16_t val)
-{
-    uint16_t romask = lduw_le_p(&s->romask[addr]);
-    uint16_t w1cmask = lduw_le_p(&s->w1cmask[addr]);
-    uint16_t oldval = lduw_le_p(&s->mmior[addr]);
-    stw_le_p(&s->mmior[addr],
-            ((oldval & romask) | (val & ~romask)) & ~(val & w1cmask));
-}
-
-static void amdvi_writel(AMDVIState *s, hwaddr addr, uint32_t val)
-{
-    uint32_t romask = ldl_le_p(&s->romask[addr]);
-    uint32_t w1cmask = ldl_le_p(&s->w1cmask[addr]);
-    uint32_t oldval = ldl_le_p(&s->mmior[addr]);
-    stl_le_p(&s->mmior[addr],
-            ((oldval & romask) | (val & ~romask)) & ~(val & w1cmask));
-}
-
-static void amdvi_writeq(AMDVIState *s, hwaddr addr, uint64_t val)
-{
-    uint64_t romask = ldq_le_p(&s->romask[addr]);
-    uint64_t w1cmask = ldq_le_p(&s->w1cmask[addr]);
-    uint32_t oldval = ldq_le_p(&s->mmior[addr]);
-    stq_le_p(&s->mmior[addr],
-            ((oldval & romask) | (val & ~romask)) & ~(val & w1cmask));
-}
-
-/* OR a 64-bit register with a 64-bit value */
-static bool amdvi_test_mask(AMDVIState *s, hwaddr addr, uint64_t val)
-{
-    return amdvi_readq(s, addr) | val;
-}
-
-/* OR a 64-bit register with a 64-bit value storing result in the register */
-static void amdvi_assign_orq(AMDVIState *s, hwaddr addr, uint64_t val)
-{
-    amdvi_writeq_raw(s, addr, amdvi_readq(s, addr) | val);
-}
-
-/* AND a 64-bit register with a 64-bit value storing result in the register */
-static void amdvi_assign_andq(AMDVIState *s, hwaddr addr, uint64_t val)
-{
-   amdvi_writeq_raw(s, addr, amdvi_readq(s, addr) & val);
-}
 
 static void amdvi_generate_msi_interrupt(AMDVIState *s)
 {
