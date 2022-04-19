108cbe3bdf64e48893cc7967467d413fb75df485
Michael Roth <michael.roth@amd.com>
2022-04-18 23:03:37 -0500

    *kvm: Allow -EPERM from vcpu_run since KVM_EXIT_MEMORY_FAULT expects it
    Some UPM development kernels return -1 when forwarding
    KVM_EXIT_MEMORY_FAULT to userspace.
    
    With newer kernels this may no longer be necessary, but keep it for now.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -3025,6 +3025,10 @@ int kvm_cpu_exec(CPUState *cpu)
                 ret = EXCP_INTERRUPT;
                 break;
             }
+            if (run_ret == -1) {
+                run_ret = 0;
+                goto skip;
+            }
             fprintf(stderr, "error: kvm run failed %s\n",
                     strerror(-run_ret));
 #ifdef TARGET_PPC
@@ -3039,6 +3043,7 @@ int kvm_cpu_exec(CPUState *cpu)
             break;
         }
 
+skip:
         trace_kvm_run_exit(cpu->cpu_index, run->exit_reason);
         switch (run->exit_reason) {
         case KVM_EXIT_IO:
