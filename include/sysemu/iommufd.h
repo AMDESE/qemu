ea65799341600e29e746f451788e4eda29ddcfea
Yi Liu <yi.l.liu@intel.com>
2022-06-29 03:12:49 -0700

    backend/iommufd: Add helper for nesting support
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>
    Signed-off-by: Yi Sun <yi.y.sun@linux.intel.com>

--- include/sysemu/iommufd.h
+++ include/sysemu/iommufd.h
@@ -5,6 +5,7 @@
 #include "qemu/thread.h"
 #include "exec/hwaddr.h"
 #include "exec/cpu-common.h"
+#include <linux/iommufd.h>
 
 #define TYPE_IOMMUFD_BACKEND "iommufd"
 OBJECT_DECLARE_TYPE(IOMMUFDBackend, IOMMUFDBackendClass,
@@ -36,6 +37,7 @@ void iommufd_backend_disconnect(IOMMUFDBackend *be);
 
 int iommufd_backend_get_ioas(IOMMUFDBackend *be, uint32_t *ioas_id);
 void iommufd_backend_put_ioas(IOMMUFDBackend *be, uint32_t ioas_id);
+void iommufd_backend_free_id(int fd, uint32_t id);
 int iommufd_backend_unmap_dma(IOMMUFDBackend *be, uint32_t ioas,
                               hwaddr iova, ram_addr_t size);
 int iommufd_backend_map_dma(IOMMUFDBackend *be, uint32_t ioas, hwaddr iova,
@@ -43,5 +45,10 @@ int iommufd_backend_map_dma(IOMMUFDBackend *be, uint32_t ioas, hwaddr iova,
 int iommufd_backend_copy_dma(IOMMUFDBackend *be, uint32_t src_ioas,
                              uint32_t dst_ioas, hwaddr iova,
                              ram_addr_t size, bool readonly);
-
+int iommufd_backend_alloc_hwpt(int iommufd, uint32_t dev_id,
+                               uint32_t pt_id, uint32_t hwpt_type,
+                               uint32_t len, void *data_ptr,
+                               uint32_t *out_hwpt);
+int iommufd_backend_invalidate_cache(int iommufd, uint32_t hwpt_id,
+                                     uint32_t len, void *data_ptr);
 #endif
