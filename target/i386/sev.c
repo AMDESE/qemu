9e33aecd8f0cfa4300d6a39040abae7b2b6460b4
Michael Roth <michael.roth@amd.com>
2022-07-31 18:43:15 -0500

    *kvm: Add trace_kvm_vmgexit_psc
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- target/i386/sev.c
+++ target/i386/sev.c
@@ -2255,6 +2255,8 @@ static int kvm_handle_vmgexit_msr_protocol(__u64 *ghcb_msr)
     op = (*ghcb_msr & GHCB_MSR_PSC_OP_MASK) >> GHCB_MSR_PSC_OP_POS;
     gfn = (*ghcb_msr & GHCB_MSR_PSC_GFN_MASK) >> GHCB_MSR_PSC_GFN_POS;
 
+    trace_kvm_vmgexit_psc_msr_proto(gfn, 0x1000, op == GHCB_MSR_PSC_OP_PRIVATE);
+
     ret = kvm_convert_memory(gfn << TARGET_PAGE_BITS, TARGET_PAGE_SIZE,
                              op == GHCB_MSR_PSC_OP_PRIVATE);
 
@@ -2299,6 +2301,10 @@ int kvm_handle_vmgexit(__u64 *ghcb_msr, uint8_t *error)
         int ret;
 
         private = entry->operation == 1;
+
+        trace_kvm_vmgexit_psc(entry->gfn, entry->pagesize ? 0x200000 : 0x1000,
+                              entry->cur_page, entry->operation, private);
+
         ret = kvm_convert_memory(entry->gfn * 0x1000, entry->pagesize ? 0x200000 : 0x1000,
                                  private);
         if (ret) {
