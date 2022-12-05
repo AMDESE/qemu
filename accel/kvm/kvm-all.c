intel-staging/qemu-tdx

KVM: print flag when reporting error of KVM_SET_USER_MEMORY_REGION
Now KVM_SET_USER_MEMORY_REGION supports PRIVATE flag.  When reporting error
of KVM_SET_USER_MEMORY_REGION, print flags for debug.

Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

accel/kvm/kvm-all.c
@@ -395,9 +395,13 @@ err:
                               mem.region.userspace_addr, ret);
     if (ret < 0) {
         error_report("%s: KVM_SET_USER_MEMORY_REGION failed, slot=%d,"
-                     " start=0x%" PRIx64 ", size=0x%" PRIx64 ": %s",
+                     " start=0x%" PRIx64 ", size=0x%" PRIx64 ","
+                     " flags=0x%" PRIx32 ","
+                     " private_fd=%" PRId32 ", private_offset=0x%" PRIx64 ": %s",
                      __func__, mem.region.slot, slot->start_addr,
-                     (uint64_t)mem.region.memory_size, strerror(errno));
+                     (uint64_t)mem.region.memory_size, mem.region.flags,
+                     mem.private_fd, (uint64_t)mem.private_offset,
+                     strerror(errno));
     }
     return ret;
 }

AMDESE/qemu

accel/kvm/kvm-all.c
@@ -315,9 +315,13 @@ err:
                               mem.region.userspace_addr, ret);
     if (ret < 0) {
         error_report("%s: KVM_SET_USER_MEMORY_REGION failed, slot=%d,"
-                     " start=0x%" PRIx64 ", size=0x%" PRIx64 ": %s",
+                     " start=0x%" PRIx64 ", size=0x%" PRIx64 ","
+                     " flags=0x%" PRIx32 ","
+                     " restricted_fd=%" PRId32 ", restricted_offset=0x%" PRIx64 ": %s",
                      __func__, mem.region.slot, slot->start_addr,
-                     (uint64_t)mem.region.memory_size, strerror(errno));
+                     (uint64_t)mem.region.memory_size, mem.region.flags,
+                     mem.restricted_fd, (uint64_t)mem.restricted_offset,
+                     strerror(errno));
     }
     return ret;
 }