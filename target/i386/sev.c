838962b9258cc4236bfb01d884fde526544cbc32
Tom Lendacky <thomas.lendacky@amd.com>
2023-07-10 14:55:27 -0500

    target/i386/sev: Check metadata type instead of SNP page type
    Switch the data type check done in snp_populate_metadata_pages() from
    the SNP page type to the metadata type. This provides improved flexibility
    going forward for detecting the type of processing required.
    
    No functional change.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- target/i386/sev.c
+++ target/i386/sev.c
@@ -1623,7 +1623,7 @@ snp_populate_metadata_pages(OvmfSevMetadata *metadata)
             exit(1);
         }
 
-        if (type == KVM_SEV_SNP_PAGE_TYPE_CPUID) {
+        if (desc->type == SEV_DESC_TYPE_CPUID) {
             ret = snp_launch_update_cpuid(desc->base, hva, desc->len, VMPL0);
         } else if (desc->type == SEV_DESC_TYPE_SNP_KERNEL_HASHES) {
             ret = snp_launch_update_kernel_hashes(desc->base, hva, desc->len, VMPL0);
