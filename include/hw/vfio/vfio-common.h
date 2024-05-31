f13ac60bf1851c239283d52259e7095e7b42b5bf
Alexey Kardashevskiy <aik@amd.com>
2024-05-31 13:04:49 +1000

    vfio/pci: Add TIO enable switch
    Add a vfio-pci property to hide the TEE-IO PCIe capability from the VM
    allowing it using the passed through device in old manner - with SWIOTLB and
    C=0 MMIO access.
    
    Later this switch will control IOMMUFD<->MEMFD binding.
    
    Signed-off-by: Alexey Kardashevskiy <aik@amd.com>

--- include/hw/vfio/vfio-common.h
+++ include/hw/vfio/vfio-common.h
@@ -153,6 +153,7 @@ typedef struct VFIODevice {
     bool needs_reset;
     bool no_mmap;
     bool ram_block_discard_allowed;
+    bool tee_io;
     OnOffAuto enable_migration;
     OnOffAuto migration_multifd_transfer;
     bool migration_events;
