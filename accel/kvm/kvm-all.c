04e0ea5387caf91a786d4351d251c6c0d29c4485
Alexey Kardashevskiy <aik@amd.com>
2025-07-02 07:52:31 +1000

    kvm/tio: Work around failed VFIO dmabuf conversion
    For TIO, VFIO exports its MMIO via "dmabuf" in addition to the usual
    VMA via mmap(). The KVM tries converting gmemfds and this new VFIO
    dmabuf looks like it but it does not allow the conversion in
    in the gmemfd way, hence print a warning and continue.
    
    Signed-off-by: Alexey Kardashevskiy <aik@amd.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -1461,7 +1461,13 @@ retry:
             }
             error_report("Conversion failed for guest_memfd %d offset 0x%" HWADDR_PRIx " GPA 0x%" HWADDR_PRIx " ret %d",
                          rb->guest_memfd, gmem_start, start, ret);
-            vm_stop(RUN_STATE_INTERNAL_ERROR);
+            if (ret == -ENOTTY) {
+                printf("VFIO? Cannot convert for VFIO\n");
+                vm_stop(RUN_STATE_INTERNAL_ERROR);
+                ret = 0;
+            } else {
+                vm_stop(RUN_STATE_INTERNAL_ERROR);
+            }
         }
 
         start += convert.size;
