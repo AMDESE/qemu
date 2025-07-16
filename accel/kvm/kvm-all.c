8d6af36df2e6dddbb5c3bd842d6044bfc8fc193c
Alexey Kardashevskiy <aik@amd.com>
2025-07-16 13:56:45 +1000

    kvm/gmemfd: Add traces
    Signed-off-by: Alexey Kardashevskiy <aik@amd.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -377,7 +377,12 @@ static int kvm_set_user_memory_region(KVMMemoryListener *kml, KVMSlot *slot, boo
         }
     }
     mem.memory_size = slot->memory_size;
+    trace_kvm_set_user_memory(mem.slot >> 16, (uint16_t)mem.slot, mem.flags,
+                              mem.guest_phys_addr, mem.memory_size,
+                              mem.userspace_addr, mem.guest_memfd,
+                              mem.guest_memfd_offset, 0xFFFF);
     if (kvm_guest_memfd_supported) {
+//    if (kvm_guest_memfd_supported && slot->guest_memfd >= 0) {
         ret = kvm_vm_ioctl(s, KVM_SET_USER_MEMORY_REGION2, &mem);
     } else {
         ret = kvm_vm_ioctl(s, KVM_SET_USER_MEMORY_REGION, &mem);
@@ -1451,6 +1456,7 @@ static int gmem_set_shareability(hwaddr start, uint64_t size, bool shareable)
         convert.size = (gmem_start + size > mrs.mr->size) ? mrs.mr->size - gmem_start : size;
 
 retry:
+        trace_kvm_gmem_ioctl(rb->guest_memfd, start, size, shareable, gmem_start, convert.offset, convert.size);
         ret = kvm_gmem_ioctl(rb->guest_memfd, shareable ? KVM_GMEM_CONVERT_SHARED : KVM_GMEM_CONVERT_PRIVATE, &convert);
         if (ret) {
             if (ret == -EAGAIN) {
