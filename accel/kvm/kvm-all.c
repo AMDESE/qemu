717495103eb0d32fa60fccf76fe9e7eb69e4f672
Ashish Kalra <ashish.kalra@amd.com>
2025-09-18 22:10:35 +0000

    accel/kvm: Fix kvm_convert_memory calls crossing memory regions
    Page conversion call can span multiple memory regions, potentially
    resulting in a conversion failure if the memory range being converted
    extends beyond the boundaries of the referenced memory region.
    
    Handle the case of page conversion call straddling across memory
    regions.
    
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -3068,6 +3068,8 @@ static void kvm_eat_signals(CPUState *cpu)
 int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private)
 {
     MemoryRegionSection section;
+    hwaddr convert_start;
+    hwaddr convert_size;
     ram_addr_t offset;
     MemoryRegion *mr;
     RAMBlock *rb;
@@ -3085,6 +3087,11 @@ int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private)
         return ret;
     }
 
+    /*
+     * Page conversions can span multiple memory regions, for example, if two
+     * memory backends are added to support two different NUMA nodes/policies.
+     */
+next_memory_region:
     section = memory_region_find(get_system_memory(), start, size);
     mr = section.mr;
     if (!mr) {
@@ -3135,6 +3142,10 @@ int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private)
     addr = memory_region_get_ram_ptr(mr) + section.offset_within_region;
     rb = qemu_ram_block_from_host(addr, false, &offset);
 
+    convert_start = section.offset_within_region;
+    convert_size = (convert_start + size > mr->size) ?
+                   mr->size - convert_start : size;
+
     /*
      * Discarding after conversion is generally done to avoid doubling of
      * memory usage, but this only ends up hurting performance in the case
@@ -3155,11 +3166,17 @@ int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private)
         }
         ret = ram_block_discard_is_disabled()
               ? 0
-              : ram_block_discard_range(rb, offset, size);
+              : ram_block_discard_range(rb, offset, convert_size);
     } else {
         ret = ram_block_discard_is_disabled()
               ? 0
-              : ram_block_discard_guest_memfd_range(rb, offset, size);
+              : ram_block_discard_guest_memfd_range(rb, offset, convert_size);
+    }
+
+    if (size - convert_size) {
+        start += convert_size;
+        size -= convert_size;
+        goto next_memory_region;
     }
 
 out_unref:
