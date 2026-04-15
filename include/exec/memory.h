AMDESE/qemu

commit d911434564dfce93b8918b88b5394c8df0ef8dd2

*Don't use hugepages for BIOS/ROM regions
Signed-off-by: Michael Roth <michael.roth@amd.com>

include/exec/memory.h
@@ -245,6 +245,7 @@ typedef struct IOMMUTLBEvent {
 
 /* RAM can be private that has kvm guest memfd backend */
 #define RAM_GUEST_MEMFD   (1 << 12)
+#define RAM_GUEST_MEMFD_NOHUGE   (1 << 13)
 
 /*
  * In RAMBlock creation functions, if MAP_SHARED is 0 in the flags parameter,

system/physmem.c
@@ -1930,7 +1930,8 @@ static void ram_block_add(RAMBlock *new_block, Error **errp)
             gmem_flags |= GUEST_MEMFD_FLAG_MMAP;
 
         if (current_machine->cgs &&
-            current_machine->cgs->gmem_allocator == GUEST_MEM_FD_ALLOCATOR_HUGETLB) {
+            current_machine->cgs->gmem_allocator == GUEST_MEM_FD_ALLOCATOR_HUGETLB &&
+            !(new_block->flags & RAM_GUEST_MEMFD_NOHUGE)) {
             gmem_flags |= GUEST_MEMFD_FLAG_HUGETLB;
 
             switch (current_machine->cgs->gmem_page_size) {

system/memory.c
@@ -3723,7 +3723,7 @@ bool memory_region_init_ram_guest_memfd(MemoryRegion *mr,
     DeviceState *owner_dev;
 
     if (!memory_region_init_ram_flags_nomigrate(mr, owner, name, size,
-                                                RAM_GUEST_MEMFD, errp)) {
+                                                RAM_GUEST_MEMFD|RAM_GUEST_MEMFD_NOHUGE, errp)) {
         return false;
     }
     /* This will assert if owner is neither NULL nor a DeviceState.

commit a81866fb52d1be3a2846fa5eed022da090ae5441

exec/memory: fix bit collision for RAM_GUEST_MEMFD_NOHUGE
RAM_GUEST_MEMFD_NOHUGE was mistakenly defined as (1 << 13), which is
already used by RAM_PRIVATE. As a result, enabling NOHUGE also toggled
RAM_PRIVATE, leading to incorrect private mapping semantics and hitting
asserts in the RAM allocation paths.

Assign RAM_GUEST_MEMFD_NOHUGE a unique bit (1 << 14) and whitelist it
in qemu_ram_alloc_from_fd(), qemu_ram_alloc_internal(), and
qemu_ram_alloc() assertion masks.

This restores correct flag semantics and makes guest_memfd without
hugepages usable without tripping assertions.

Fixes: d911434564df (Don't use hugepages for BIOS/ROM regions)
Signed-off-by: Yipeng Yin <yinyipeng@bytedance.com>

include/exec/memory.h
@@ -245,7 +245,7 @@ typedef struct IOMMUTLBEvent {
 
 /* RAM can be private that has kvm guest memfd backend */
 #define RAM_GUEST_MEMFD   (1 << 12)
-#define RAM_GUEST_MEMFD_NOHUGE   (1 << 13)
+#define RAM_GUEST_MEMFD_NOHUGE   (1 << 14)
 
 /*
  * In RAMBlock creation functions, if MAP_SHARED is 0 in the flags parameter,

system/physmem.c
@@ -2059,7 +2059,7 @@ RAMBlock *qemu_ram_alloc_from_fd(ram_addr_t size, ram_addr_t max_size,
     assert((ram_flags & ~(RAM_SHARED | RAM_PMEM | RAM_NORESERVE |
                           RAM_PROTECTED | RAM_NAMED_FILE | RAM_READONLY |
                           RAM_READONLY_FD | RAM_GUEST_MEMFD |
-                          RAM_RESIZEABLE)) == 0);
+                          RAM_RESIZEABLE | RAM_GUEST_MEMFD_NOHUGE)) == 0);
     assert(max_size >= size);
 
     if (xen_enabled()) {
@@ -2241,7 +2241,7 @@ RAMBlock *qemu_ram_alloc_internal(ram_addr_t size, ram_addr_t max_size,
     ram_flags &= ~RAM_PRIVATE;
 
     assert((ram_flags & ~(RAM_SHARED | RAM_RESIZEABLE | RAM_PREALLOC |
-                          RAM_NORESERVE | RAM_GUEST_MEMFD)) == 0);
+                          RAM_NORESERVE | RAM_GUEST_MEMFD | RAM_GUEST_MEMFD_NOHUGE)) == 0);
     assert(!host ^ (ram_flags & RAM_PREALLOC));
     assert(max_size >= size);
 
@@ -2339,7 +2339,7 @@ RAMBlock *qemu_ram_alloc(ram_addr_t size, uint32_t ram_flags,
                          MemoryRegion *mr, Error **errp)
 {
     assert((ram_flags & ~(RAM_SHARED | RAM_NORESERVE | RAM_GUEST_MEMFD |
-                          RAM_PRIVATE)) == 0);
+                          RAM_PRIVATE | RAM_GUEST_MEMFD_NOHUGE)) == 0);
     return qemu_ram_alloc_internal(size, size, NULL, NULL, ram_flags, mr, errp);
 }
 